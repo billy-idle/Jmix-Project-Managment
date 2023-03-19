ef681188076d34c1e8ef993d9ef3169dc2d8ac15
gguzmans <gguzmans@emeal.nttdata.com>
2023-03-19 17:01:19 -0500

    Se agregan las entidades Project, Task, User. Se genera el changelog liquibase

--- src/main/java/com/company/jmixpm/entity/User.java
+++ src/main/java/com/company/jmixpm/entity/User.java
@@ -6,152 +6,106 @@ import io.jmix.core.metamodel.annotation.DependsOnProperties;
 import io.jmix.core.metamodel.annotation.InstanceName;
 import io.jmix.core.metamodel.annotation.JmixEntity;
 import io.jmix.security.authentication.JmixUserDetails;
-import org.springframework.security.core.GrantedAuthority;
-
-import javax.persistence.*;
-import javax.validation.constraints.Email;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Objects;
 import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+import javax.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 
+@Getter
+@Setter
 @JmixEntity
 @Entity
 @Table(name = "USER_", indexes = {
-        @Index(name = "IDX_USER__ON_USERNAME", columnList = "USERNAME", unique = true)
+    @Index(name = "IDX_USER__ON_USERNAME", columnList = "USERNAME", unique = true)
 })
 public class User implements JmixUserDetails {
 
-    @Id
-    @Column(name = "ID")
-    @JmixGeneratedValue
-    private UUID id;
+  @Column(name = "USERNAME", nullable = false)
+  protected String username;
+  @SystemLevel
+  @Column(name = "PASSWORD")
+  protected String password;
+  @Column(name = "FIRST_NAME")
+  protected String firstName;
+  @Column(name = "LAST_NAME")
+  protected String lastName;
+  @Email
+  @Column(name = "EMAIL")
+  protected String email;
+  @Column(name = "ACTIVE")
+  protected Boolean active = true;
+  @Transient
+  protected Collection<? extends GrantedAuthority> authorities;
+  @Id
+  @Column(name = "ID")
+  @JmixGeneratedValue
+  private UUID id;
+  @Version
+  @Column(name = "VERSION", nullable = false)
+  private Integer version;
 
-    @Version
-    @Column(name = "VERSION", nullable = false)
-    private Integer version;
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities != null ? authorities : Collections.emptyList();
+  }
 
-    @Column(name = "USERNAME", nullable = false)
-    protected String username;
+  @Override
+  public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+    this.authorities = authorities;
+  }
 
-    @SystemLevel
-    @Column(name = "PASSWORD")
-    protected String password;
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
-    @Column(name = "FIRST_NAME")
-    protected String firstName;
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
 
-    @Column(name = "LAST_NAME")
-    protected String lastName;
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
 
-    @Email
-    @Column(name = "EMAIL")
-    protected String email;
+  @Override
+  public boolean isEnabled() {
+    return Boolean.TRUE.equals(active);
+  }
 
-    @Column(name = "ACTIVE")
-    protected Boolean active = true;
+  @InstanceName
+  @DependsOnProperties({"firstName", "lastName", "username"})
+  public String getDisplayName() {
+    return String.format("%s %s [%s]", (firstName != null ? firstName : ""),
+        (lastName != null ? lastName : ""), username).trim();
+  }
 
-    @Transient
-    protected Collection<? extends GrantedAuthority> authorities;
-
-    public UUID getId() {
-        return id;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public void setId(UUID id) {
-        this.id = id;
+    if (!(o instanceof User)) {
+      return false;
     }
+    User user = (User) o;
+    return id.equals(user.id);
+  }
 
-    public Integer getVersion() {
-        return version;
-    }
-
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities != null ? authorities : Collections.emptyList();
-    }
-
-    @Override
-    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
-        this.authorities = authorities;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return Boolean.TRUE.equals(active);
-    }
-
-    @InstanceName
-    @DependsOnProperties({"firstName", "lastName", "username"})
-    public String getDisplayName() {
-        return String.format("%s %s [%s]", (firstName != null ? firstName : ""),
-                (lastName != null ? lastName : ""), username).trim();
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
